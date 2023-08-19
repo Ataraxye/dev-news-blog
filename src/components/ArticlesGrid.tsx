67edd420fee00b5a5442bb8322826c9c6b09f9ba
Teodora Hovi <teodora.hovi@gmail.com>
2023-08-20 00:21:09 +0200

    Added placeholder text when there are no elements in a category

--- src/components/ArticlesGrid.tsx
+++ src/components/ArticlesGrid.tsx
@@ -8,15 +8,22 @@ interface ArticlesGridProps {
 const ArticlesGrid = ({ articles }: ArticlesGridProps) => {
   return (
     <div className="row row-cols-1 row-cols-md-1 row-cols-xl-3 gx-4">
-      {articles.map((article, index) => (
-        <ArticleThumbnail
-          title={article.title}
-          type={article.type}
-          tags={article.tags}
-          preview={article.preview}
-          image={article.image}
-        />
-      ))}
+      {articles.length > 0 ? (
+        articles.map((article, index) => (
+          <ArticleThumbnail
+            title={article.title}
+            type={article.type}
+            tags={article.tags}
+            preview={article.preview}
+            image={article.image}
+          />
+        ))
+      ) : (
+        <p>
+          On dirait qu'il n'y a pas encore d'articles dans cette catégorie !
+          Revenez plus tard !
+        </p>
+      )}
     </div>
   );
 };
